7131af1c89c1a6b13a9fdeef2df31101828b5753
vamsikrd <53194809+vamsikrd@users.noreply.github.com>
2019-11-03 13:40:46 +0530

    _nav

--- Avatar/Assets/Scripts/AvatarController.cs
+++ Avatar/Assets/Scripts/AvatarController.cs
@@ -1,18 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class AvatarController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Cache References//
+    private NavMeshAgent _nav;
+    private Animator _anim;
+
+    //Private Variables//
+    
+
+    //Public Variables//
+    public LayerMask ground;
+    
+
     void Start()
     {
-        
+        _nav = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        ClickMovement();
     }
-}
+
+    private void ClickMovement()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                _nav.SetDestination(hit.point);
+            }
+        }
+        _anim.SetBool("isRunning", _nav.hasPath);
+    }
+} //Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Avatar/Assets/Scripts/GaurdController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class GaurdController : MonoBehaviour
+{
+    //Cache Variables//
+    private NavMeshAgent _nav;
+    private Animator _anim;
+
+    //Private Variables//
+    [SerializeField] private List<Tomb> tombs = new List<Tomb>();
+    [SerializeField] private Transform target;
+    private bool gotNearest = false;
+    private float nearestTomb;
+    //Public Variables//
+
+    private void Awake()
+    {
+       
+    }
+
+
+    private void Start()
+    {
+        _nav = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
+        nearestTomb = Vector2.Distance(transform.position, FindObjectOfType<Tomb>().transform.position);
+       
+    }
+
+
+    private void Update()
+    {
+        GettingTheNearestTomb();
+        
+    }
+
+    private void GettingTheNearestTomb()
+    {
+        if (!gotNearest)
+        {
+            foreach (Tomb tomb in tombs)
+            {
+                if (nearestTomb >= Vector2.Distance(transform.position, tomb.transform.position))
+                {
+                    target = tomb.transform;
+                }
+            }
+            gotNearest = true;
+        }
+    }
+}
