71446491cead616ac1c322166e216df938f7a983
vamsikrd <53194809+vamsikrd@users.noreply.github.com>
2019-12-18 12:33:06 +0530

    Player and AI

---
+++ Defend-Sp4/Assets/Scripts/AI/AIState.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIState : MonoBehaviour
+{
+    private Animator _anim;
+    private NavMeshAgent _navAgent;
+
+    [SerializeField] private Transform targetTower = null;
+    [SerializeField] private Transform defender = null;
+
+    public int currentTowerIndex;
+    public int totalTowers;
+    public List<Transform> _towers = new List<Transform>();
+    public GameObject[] towers;
+    public bool towerSelected = false;
+    public bool canAttack = false;
+    [Range(0f, 5f)] public float _attackRange = 2f;
+
+    private void Awake()
+    {
+        towers = GameObject.FindGameObjectsWithTag("Tower");
+        foreach (GameObject tower in towers)
+        {
+            if (!_towers.Contains(tower.transform))
+            {
+                _towers.Add(tower.transform);
+            }
+        }
+        defender = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (!towerSelected)
+              SelectingATower();
+
+        else
+        if(towerSelected)
+        {
+            if(canAttack)
+            {
+                Attack(targetTower);
+            }
+        }
+    }
+
+    private void SelectingATower()
+    {
+        if (canAttack) return;
+        totalTowers = _towers.Count;
+        currentTowerIndex = Random.Range(0, totalTowers);
+        if(currentTowerIndex < totalTowers)
+        {
+            targetTower = _towers[currentTowerIndex];
+            towerSelected = true;
+            canAttack = true;
+            print("!");
+        }
+    }
+
+    private void Attack(Transform currentTarget)
+    {
+        if(currentTarget != null)
+        {
+            if(Vector3.Distance(transform.position,currentTarget.position) <= _attackRange)
+            {
+                _navAgent.ResetPath();
+                print("Attack");
+            }
+            else
+            {
+                _navAgent.SetDestination(currentTarget.position);
+            }
+        }
+        else
+        if(currentTarget == null)
+        {
+            _towers.Remove(targetTower);
+            canAttack = false;
+            SelectingATower();
+        }
+    }
+
+   
+}
---
+++ Defend-Sp4/Assets/Scripts/Player/DefenderState.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class DefenderState : MonoBehaviour
+{
+    //Inspector Assigned
+    [SerializeField] [Range(1f,10f)] private float attackRange = 5f;
+    [SerializeField] private LayerMask rayLayer;
+    [SerializeField] [Range(0f, 1f)] float _rotationSpeed = 0.2f;
+    [SerializeField] private Transform arrowShootPoint;
+    
+    //Protected
+    protected Animator        _animator;
+    protected NavMeshAgent    _navAgent;
+    protected int _isWalkingHash = Animator.StringToHash("isRunning");
+    protected int _isAttackingHash = Animator.StringToHash("isAttacking");
+
+    //Private
+    private GameObject currentTarget = null;
+
+    //Public
+    public bool goingForAttack = false;
+
+    protected virtual void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    protected virtual void Update()
+    {
+        ProjectRaycast();
+        if (goingForAttack) CanAttack();
+    }
+
+
+    protected virtual void Movement(RaycastHit hitInfo)
+    {
+        if(hitInfo.point != Vector3.zero)
+        {
+            _navAgent.Resume();
+            _navAgent.SetDestination(hitInfo.point);
+        }
+    }
+
+    protected virtual void ProjectRaycast()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray,out hitInfo))
+            {
+                if(hitInfo.collider.CompareTag("Enemy"))
+                {
+                    Attack(hitInfo);
+                }
+                else
+                {
+                    Movement(hitInfo);
+                    goingForAttack = false;
+                }
+            }
+        }
+    }
+
+    protected virtual void Attack(RaycastHit hitInfo)
+    {
+       GameObject newtarget = hitInfo.collider.gameObject;
+       if(currentTarget != newtarget)
+        {
+            currentTarget = newtarget;
+        }
+        goingForAttack = true;
+    }
+
+    protected virtual void CanAttack()
+    {
+        if (Vector3.Distance(transform.position, currentTarget.transform.position) <= attackRange)
+        {
+            _navAgent.Stop();
+            Vector3 direction = currentTarget.transform.position - transform.position;
+            direction.y = 0f;
+            direction = direction.normalized;
+            Quaternion rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 1 *_rotationSpeed);
+            _animator.SetTrigger(_isAttackingHash);
+            _animator.SetBool(_isWalkingHash, false);
+        }
+        else
+        {
+            _navAgent.Resume();
+            _animator.SetBool(_isWalkingHash, true);
+            _navAgent.SetDestination(currentTarget.transform.position);
+        }
+    }
+
+    public virtual void ShootArrow(GameObject arrowPrefab)
+    {
+        if (arrowPrefab != null)
+        {
+            GameObject arrow = Instantiate(arrowPrefab);
+            Vector3 arrowPosition = arrowShootPoint.position;
+            arrow.transform.position = arrowPosition;
+            arrow.transform.rotation = transform.rotation;
+        }
+    }
+}
